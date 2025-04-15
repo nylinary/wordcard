e3ae3d179455f2ea12958cdf7533c7349402c77f
Nikita Sysoev <nylinary.work@yandex.com>
2025-04-15 14:59:27 +0300

    feat: implement custom UserAdmin for enhanced user management in admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,41 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    ordering = ("created", "uuid")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
