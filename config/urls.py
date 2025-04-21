231f0bc7569710f149c349701ccdcc0608532781
Nikita Sysoev <nylinary.work@yandex.com>
2025-04-21 17:12:53 +0300

    feat: add static file serving for media files in development

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -8,3 +10,6 @@ urlpatterns = [
     path("words/", include("apps.words.urls")),
     path("accounts/", include("allauth.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
